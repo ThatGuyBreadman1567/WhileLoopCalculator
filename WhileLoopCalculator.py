bd5303548a1aed7a5de36c83b29e1b0be988097d
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-12-04 11:14:49 -0500

    Update WhileLoopCalculator.py

--- WhileLoopCalculator.py
+++ WhileLoopCalculator.py
@@ -1,10 +1,28 @@
 # Ben Readman
 # While loop Calculator
-go = 'yes'
+x = 0
+go = 'y'
 num = int(input("Please enter the first number: "))
-min = num
-max = num
-while go == 'yes':
-    num = int(input("Please enter the next number: "))
-    if min > num:
-        min = num
+minnum = num
+maxnum = num
+while go == 'y':
+    num2 = int(input("Please enter the next number: "))
+    avrg = num + num2
+    num = num2
+    if minnum > num2:
+        minnum = num2
+    else:
+        minnum = minnum
+    if maxnum < num2:
+        maxnum = num2
+    else:
+        maxnum = maxnum
+    print()
+    go = input("Would you like to enter another number (y/n)? ")
+    x +=1
+if go == 'n':
+    avrg = avrg/x
+    print()
+    print("Okay, the minimum is ",minnum,"\nThe maximum is ",maxnum,"\nThe average is ",avrg,sep='')
+else:
+    go = input("Please enter a valid answer (y/n): ")
